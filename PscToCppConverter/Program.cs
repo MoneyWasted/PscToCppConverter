3661f1bc0ee3c560feb42ef90fc05b70443bcf11
MoneyWasted <45946092+MoneyWasted@users.noreply.github.com>
2023-12-26 15:08:39 -0500

    Initial Commit

---
+++ PscToCppConverter/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+using System.Text;
+using System.Numerics;
+
+class PscToCppConverter
+{
+    public static void ConvertPscToCpp(string inputFilePath)
+    {
+        string outputFilePath = Path.ChangeExtension(inputFilePath, ".h");
+
+        StringBuilder cppCode = new StringBuilder();
+
+        XmlDocument doc = new XmlDocument();
+        doc.Load(inputFilePath);
+
+        XmlNodeList structDefs = doc.GetElementsByTagName("structdef");
+
+        foreach (XmlNode structDef in structDefs)
+        {
+            string structName = structDef.Attributes["name"].Value;
+            cppCode.AppendLine($"struct {structName}");
+            cppCode.AppendLine("{");
+
+            foreach (XmlNode child in structDef.ChildNodes)
+            {
+                if (child.Name == "array")
+                {
+                    string arrayType = child.Attributes["type"].Value;
+                    string memberName = child.Attributes["name"].Value;
+
+                    XmlNode structNode = child.ChildNodes[0];
+                    string arrayElementTypeName = structNode.Attributes["type"].Value;
+                    
+                    cppCode.AppendLine($"    {arrayType}<{arrayElementTypeName}> {memberName};");
+                }
+                else
+                {
+                    string memberType = child.Name;
+                    string memberName = child.Attributes["name"].Value;
+                    cppCode.AppendLine($"    {memberType} {memberName};");
+                }
+            }
+
+            cppCode.AppendLine("};");
+            cppCode.AppendLine();
+        }
+
+        File.WriteAllText(outputFilePath, cppCode.ToString());
+    }
+
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: PscToCppConverter <inputFilePath>");
+            return;
+        }
+
+        string inputFilePath = args[0];
+
+        ConvertPscToCpp(inputFilePath);
+
+        Console.WriteLine($"Conversion completed. Output file: {Path.ChangeExtension(inputFilePath, ".h")}");
+    }
+}
